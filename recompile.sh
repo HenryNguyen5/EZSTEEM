f07195e55b30f3d45863e0123438815ab1d90a80
omotherhen <ngu.henry@hotmail.com>
2016-07-21 19:05:51 -0400

    Update recompile.sh

--- recompile.sh
+++ recompile.sh
@@ -2,7 +2,7 @@
 #made by steemit user omotherhen
 cd ~/steem
 git checkout v0.11.0 && rm -f CMakeCache.txt && make clean 
-cmake -DCMAKE_BUILD_TYPE=Release-DLOW_MEMORY_NODE=ON . && make -j 3
+cmake -DCMAKE_BUILD_TYPE=Release-DLOW_MEMORY_NODE=ON . && make
 clear
 
 cd ~/steem/programs/steemd
