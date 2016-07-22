d13cf9c0088cef969d42b0b3d08cf9b07b4d1410
omotherhen <ngu.henry@hotmail.com>
2016-07-22 12:22:04 -0400

    Upgraded steem checkout to v0.12.0

--- recompile.sh
+++ recompile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #made by steemit user omotherhen
 cd ~/steem
-git checkout v0.11.0 && rm -f CMakeCache.txt && make clean 
+git checkout v0.12.0 && rm -f CMakeCache.txt && make clean 
 cmake -DCMAKE_BUILD_TYPE=Release-DLOW_MEMORY_NODE=ON . && make
 clear
 
