469dd7c382ea326b28f3dba4ecdf6cb83ed4ca4e
omotherhen <ngu.henry@hotmail.com>
2016-07-23 10:17:15 -0700

    Added git fetch to update repos

--- bin/recompile.sh
+++ bin/recompile.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #made by steemit user omotherhen
 cd ~/steem
-git checkout v0.12.1 && rm -f CMakeCache.txt && make clean 
-cmake -DCMAKE_BUILD_TYPE=Release-DLOW_MEMORY_NODE=ON . && make
-clear
+git fetch
+git checkout v0.12.1
+rm -f CMakeCache.txt
+make -s clean > /dev/null
+cmake -DCMAKE_BUILD_TYPE=Release-DLOW_MEMORY_NODE=ON .
+echo -e "\e[5mCompiling your Steem installation, this may take a while"   
+make -s
+echo -e "\e[25mDone!"
+exit 0
 
-cd ~/steem/programs/steemd
-./steemd
