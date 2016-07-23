b3ceea1ca93be7e075189463e4a942f1a9bf8f36
omotherhen <ngu.henry@hotmail.com>
2016-07-23 12:41:46 -0400

    Delete clonedMiner.sh

--- clonedMiner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Modifies config files for when user has n-2 miners to setup
-cd ~/steem/programs/steemd/witness_node_data_dir
-echo "Enter in how many threads you want to mine on this machine"
-read threads
-sed -i "s/mining-threads = [0-9]*/mining-threads = $threads/" config.ini
-sed -i 's/witness = /# witness =/' config.ini
-cd ..
-./steemd --replay
