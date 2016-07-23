731f92753f03791aa06e997a7f0d61fa4a49484b
omotherhen <ngu.henry@hotmail.com>
2016-07-23 09:49:52 -0700

    Deletes previous .zip files, no longer starts miner afterwards

--- bin/refreshBlkChain.sh
+++ bin/refreshBlkChain.sh
@@ -2,5 +2,5 @@
 #made by steemit user omotherhen
 
 echo "Boot-strapping blockchain for fast setup, then starting the miner!"
-cd ~/steem/programs/steemd/witness_node_data_dir/blockchain/database/ && wget http://einfachmalnettsein.de/steem-blocks-and-index.zip && unzip -o steem-blocks-and-index.zip && cd ../../../ && ./steemd --replay
+cd ~/steem/programs/steemd/witness_node_data_dir/blockchain/database/ && rm -f *.zip && wget http://einfachmalnettsein.de/steem-blocks-and-index.zip && unzip -o steem-blocks-and-index.zip 
 
