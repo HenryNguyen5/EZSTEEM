bec58406da978941d21a63988efef4b2f6e853ee
omotherhen <ngu.henry@hotmail.com>
2016-07-23 22:33:41 -0400

    Added remove command for blockchain bootstrap

--- bin/minerScript.sh
+++ bin/minerScript.sh
@@ -243,7 +243,7 @@ sed -i "s/# mining-threads =/$mining_threads/" config.ini
 
 $pnkl "Boot-strapping blockchain for fast setup, then starting the miner!"
 $whtl
-cd "$myBaseDir/steem/programs/steemd/witness_node_data_dir/blockchain/database/" && wget http://einfachmalnettsein.de/steem-blocks-and-index.zip && unzip -o steem-blocks-and-index.zip && cd ../../../ 
+cd "$myBaseDir/steem/programs/steemd/witness_node_data_dir/blockchain/database/" && wget http://einfachmalnettsein.de/steem-blocks-and-index.zip && unzip -o steem-blocks-and-index.zip && rm -f steem-blocks-and-index.zip && cd ../../../ 
 
 $pnkl "---------------------------------------------------------------------------------------"
 $pnkl "------------------------------------Starting Miner-------------------------------------"
