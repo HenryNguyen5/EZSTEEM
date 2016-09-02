8c5757dd545f2e07968d3e28e3dfd966b96f55e7
TheBearCode <alexandre.skipper@gmail.com>
2016-09-02 15:19:09 -0700

    comments added to ezWalletMenu.js

--- js/ezWalletMenu.js
+++ js/ezWalletMenu.js
@@ -4,6 +4,7 @@ var prompt = require("./node_modules/prompt");
 var helper = require("./helper.js");
 var colors = require("colors");
 
+//clear screen
 console.log('\033[2J');
 
 console.log(`
@@ -11,7 +12,7 @@ console.log(`
 --------------------Welcome to ezWallet--------------------
 -----------------------------------------------------------`.magenta);
 
-console.log(`---------------------------Menu----------------------------`.magenta);
+console.log(`\n---------------------------Menu----------------------------`.magenta);
 
 var desc = `\n What would you like to do today?
 
@@ -22,7 +23,7 @@ var desc = `\n What would you like to do today?
 `.magenta;
 
 var runMenu = function() {
-    //prompt the user
+    //prompt the user, 0-2 inclusive
     var schema = {
         properties: {
             choice: {
@@ -40,17 +41,21 @@ var runMenu = function() {
     helper.asyncLoop(-1, function(loop) {
             ezWallet.getSteemConfFile(ezWallet.getMinerInfo);
             prompt.get(schema, function(err, result) {
+		//end program
                 if (result.choice === 0) loop.break();
+		//add, remove miners
                 if (result.choice === 1) {
                     ezWallet.getSteemConfFile(function(err, rawContents) {
                         return ezWallet.modifyMinerandWitnesses(err, rawContents, loop.next);
                     });
                 }
                 if (result.choice === 2) {
+		    //transfer vests
                     ezWallet.autowithdraw(loop.next);
                 }
             });
         },
+	//calls upon exit of program
         function() {
             console.log("end");
         }
